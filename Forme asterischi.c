35eafb5cf11782dc4a1454967b6f6a7a285c8d4a
Giovanpietro <50960446+Giovanpietro@users.noreply.github.com>
2022-09-28 14:51:13 +0200

    Add files via upload

---
+++ Forme asterischi.c
@@ -0,0 +1,13 @@
+/* Forme asterischi */
+
+#include <stdio.h>
+
+int main(){
+    printf("*****    ***      *        *\n");
+    printf("*   *   *   *    ***      * *\n");
+    printf("*   *   *   *   *****    *   *\n");
+    printf("*   *   *   *     *     *     *\n");
+    printf("*   *   *   *     *      *   *\n");
+    printf("*   *   *   *     *       * *\n");
+    printf("*****    ***      *        *\n");
+}
