fce529b96e5fc330ec35344323b1c4a1271b7f67
Giovanpietro <50960446+Giovanpietro@users.noreply.github.com>
2022-09-28 16:46:56 +0200

    Add files via upload

---
+++ Dispari o pari.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int x, pari, dispari;
+
+    printf("Inserisci intero: ");
+    scanf("%d", &x);
+
+    if(x%2==0){
+        printf("%d e' pari\n", x);
+    }else{
+        printf("%d e' dispari\n", x);
+    }
+
+    return 0;
+}
