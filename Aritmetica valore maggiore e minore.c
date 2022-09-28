15ef2e1df0d4c6db08aefee359f3af93425053e8
Giovanpietro <50960446+Giovanpietro@users.noreply.github.com>
2022-09-28 13:44:41 +0200

    Add files via upload

---
+++ Aritmetica valore maggiore e minore.c
@@ -0,0 +1,45 @@
+/* Scrivete un programma che riceva in ingresso tre diversi interi
+dalla tastiera, poi stampi la somma, la media, il prodotto,
+il minore e il maggiore di questi numeri. Usare solamente la
+formula a selezione singola dell'istruzione if */
+
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c;
+    int somma, media, prodotto, minore, maggiore;
+
+    printf("Inserisci primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci secondo numero: ");
+    scanf("%d", &b);
+    printf("Inserisci terzo numero: ");
+    scanf("%d", &c);
+
+    somma=a+b+c;
+    media=somma/3;
+    prodotto=a*b*c;
+
+    printf("\n");
+
+    if(a>b && a>c){
+        printf("%d e' maggiore\n", a);
+    }else if(b>a && b>c){
+        printf("%d e' maggiore\n", b);
+    }else if(c>a && c>b){
+        printf("%d e' maggiore\n", c);
+    }
+    if(a<b && a<c){
+        printf("%d e' minore\n", a);
+    }else if(b<a && b<c){
+        printf("%d e' minore\n", b);
+    }else{
+        printf("%d e' minore\n", c);
+    }
+
+    printf("La somma e': %d\n", somma);
+    printf("La media e': %d\n", media);
+    printf("Il prodotto e': %d\n", prodotto);
+
+}
